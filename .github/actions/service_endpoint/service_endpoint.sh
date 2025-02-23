fa12e64c38ab28bf44f2b25237965d1da9116266
Ajithkumar8006 <83852404+Ajithkumar8006@users.noreply.github.com>
2025-02-23 16:41:37 +0530

    Update service_endpoint.sh

--- .github/actions/service_endpoint/service_endpoint.sh
+++ .github/actions/service_endpoint/service_endpoint.sh
@@ -6,7 +6,8 @@ CREDENTIALS_TEST="credentials.ini"
 SERVICE_ENDPOINT="service_endpoint"
 
 # Define the path to the Python script and credentials file
-SE1="$ACTION_PATH/$SERVICE_ENDPOINT/bt_csv.py"
+SE1="test1.py"
+SE2="bt_csv.py"
 
 # Check if the Python script and credentials file exist
 if [ -f "$SE1" ]; then
@@ -14,4 +15,12 @@ if [ -f "$SE1" ]; then
     python3 "$SE1" --creds "$CREDENTIALS_TEST"
 else
     echo "Error: Either $SE1 or $CREDENTIALS_TEST not found!"
+fi
+
+# Check if the Python script and credentials file exist
+if [ -f "$SE2" ]; then
+    # Run the Python script with the specified arguments
+    python3 "$SE2" --creds "$CREDENTIALS_TEST"
+else
+    echo "Error: Either $SE2 or $CREDENTIALS_TEST not found!"
 fi
