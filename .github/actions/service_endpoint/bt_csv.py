5f1cacad7d166bf243a0b04d824b5263535fc584
Ajithkumar8006 <83852404+Ajithkumar8006@users.noreply.github.com>
2025-02-24 00:02:52 +0530

    Update bt_csv.py

--- .github/actions/service_endpoint/bt_csv.py
+++ .github/actions/service_endpoint/bt_csv.py
@@ -30,8 +30,8 @@ def json_to_csv(json_file, csv_file):
     print(f"Data has been written to {csv_file}")
 
 # Example usage
-json_file = '.github/actions/service_endpoint/{app_online}_bt.json'  # Path to your JSON file
-csv_file = '.github/actions/service_endpoint/{app_online}_bt.csv'    # Desired output CSV file
+json_file = '.github/actions/service_endpoint/bt.json'  # Path to your JSON file
+csv_file = '.github/actions/service_endpoint/bt.csv'    # Desired output CSV file
 
 # Call the function to convert JSON to CSV
 json_to_csv(json_file, csv_file)
