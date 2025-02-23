d4f0c0ad0a1f158c02738d5dc2d2e54f7f33ce9c
Ajithkumar8006 <83852404+Ajithkumar8006@users.noreply.github.com>
2025-02-24 00:06:55 +0530

    Update test.py

--- .github/actions/test.py
+++ .github/actions/test.py
@@ -121,6 +121,6 @@ if __name__ == "__main__":
     #print("Downloaded JSON data: ", json.dumps(json_data, indent=4))
 
     # Save the downloaded JSON data to a file
-    with open("bt.json", "w") as json_file:
+    with open(".github/actions/service_endpoint/bt.json", "w") as json_file:
         json.dump(json_data, json_file, indent=4)
     print("JSON data saved to bt.json")
