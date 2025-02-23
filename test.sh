d0c1f8eb4f6677b619c39b349b790f1716574f92
Ajithkumar8006 <83852404+Ajithkumar8006@users.noreply.github.com>
2025-02-23 15:13:33 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,12 +5,11 @@ ACTION_PATH=".github/actions"
 
 # Define the path to the Python script and credentials file
 SCRIPT_PATH="$ACTION_PATH/service_endpoint/test1.py"
-CREDENTIALS_FILE="$ACTION_PATH/credentials.ini"
 
 # Check if the Python script and credentials file exist
-if [ -f "$SCRIPT_PATH" ] && [ -f "$CREDENTIALS_FILE" ]; then
+if [ -f "$SCRIPT_PATH" ]; then
     # Run the Python script with the specified arguments
-    python3 "$SCRIPT_PATH" --creds "$CREDENTIALS_FILE"
+    python3 "$SCRIPT_PATH" --creds credentials.ini
 else
     echo "Error: Either $SCRIPT_PATH or $CREDENTIALS_FILE not found!"
 fi
