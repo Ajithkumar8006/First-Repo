1bc2b695ac5c6e7a0c7f0c4519a3bf941c9356cf
Ajithkumar8006 <83852404+Ajithkumar8006@users.noreply.github.com>
2025-03-01 02:05:29 +0530

    Update run_appdynamics.sh

--- .github/actions/run_appdynamics.sh
+++ .github/actions/run_appdynamics.sh
@@ -4,7 +4,7 @@
 echo "Running AppDynamics API script for app: $APP_NAME with transaction: $TRANSACTION_NAME..."
 
 # Ensure the correct values are being passed to the Python script
-python appdynamics_api.py --cred "$CRED_FILE" --debug "5" --output "json" --app "Patient_Online_Services_Portal_Test" --transaction "all_business_transactions_list"
+python appdynamics_api.py --cred .github/actions/credentials.ini --debug "5" --output "json" --app "Patient_Online_Services_Portal_Test" --transaction "all_business_transactions_list"
 
 # Optionally list contents of the temp directory
 ls -l ./temp
